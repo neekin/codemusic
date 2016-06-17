c73cbe04c82304797a32c8f1f87c6790f65e1ae3
NeeKin <njgzs360@gmail.com>
2016-06-17 17:20:00 +0800

    add flask_admin

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
+from flask_admin import Admin
 import pymysql
 pymysql.install_as_MySQLdb()
 
@@ -12,6 +13,7 @@ pymysql.install_as_MySQLdb()
 moment = Moment()
 db = SQLAlchemy()
 login_manager = LoginManager()
+admin = Admin()
 login_manager.session_protection = 'base'
 # login_manager.login_view = 'auth.login'
 def create_app(config_name):
@@ -22,11 +24,13 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    
+    admin.init_app(app)
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
+    # from .admin import admin as adminManage_blueprint
+    # app.register_blueprint(adminManage_blueprint)
     # from .auth import auth as auth_blueprint
     # app.register_blueprint(auth_blueprint,url_prefix='/auth')
     
